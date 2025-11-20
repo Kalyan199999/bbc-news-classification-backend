390a92c8a340dd1b42c40805db4a9e76a3186fb0
kalyan199999 <gottapupavankalyan@gmail.com>
2025-11-20 20:09:29 +0530

    bbc-news-classification initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import joblib
+from fastapi import FastAPI 
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from util import preprocessing
+
+# Load model and vectorizer
+model_path = './models'
+try:
+    model = joblib.load(os.path.join(model_path, 'model.pkl'))
+    vectorizer = joblib.load(os.path.join(model_path, 'vectorizer.pkl'))
+except Exception as e:
+    print(f'Error Occurred: {e}')
+
+app = FastAPI( debug=True )
+
+# Enable CORS
+origins = [
+    "http://localhost:3000",
+    "https://insurence-premium-prediction-fronte.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Home route
+@app.get("/")
+def home():
+    return JSONResponse(
+        content={"message": "BBC News Classification API"}, 
+        status_code=200
+        )
+
+# Pydantic model for JSON input
+class PredictRequest(BaseModel):
+    text: str
+
+@app.post("/predict")
+def prediction(request: PredictRequest):
+    input_text = request.text
+    processed_text = preprocessing(input_text)
+    text_vector = vectorizer.transform([processed_text])
+    prediction = model.predict(text_vector)[0]
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "ok":True,
+            "message": "Prediction is successful!❤️",
+            "text": input_text,
+            "processed_text": processed_text,
+            "prediction":prediction
+        }
+    )
---
+++ util.py
@@ -0,0 +1,27 @@
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Example setup
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+# Pattern: match special characters (except letters/numbers)
+pattern = r'[^a-zA-Z0-9\s]'
+
+def preprocessing(text):
+    
+    # Replace special characters with a space
+    text = re.sub(pattern=pattern, repl=' ', string=text)
+
+    # Tokenize
+    words = word_tokenize(text)
+
+    # Remove stopwords and lemmatize
+    lemma_words = [lemmatizer.lemmatize(word) for word in words if word.lower() not in stop_words]
+
+    # Join back into string
+    res = ' '.join(lemma_words)
+
+    return res
